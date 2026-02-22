e5129f6ebb4b2ad332e8ea8c30d3c4586267d75c
Brayden Moon <brayden@binbandit.dev>
2026-02-22 15:01:12 +1100

    feat(nvim-themes): add switchable colorscheme setup

--- dotfiles/config/nvim/lua/plugins/themes.lua
+++ dotfiles/config/nvim/lua/plugins/themes.lua
@@ -1,24 +1,52 @@
+-- Change this to switch themes: "koda" | "vague" | "oxocarbon" | "yugen" | "moonfly"
+local theme = "koda"
+
+local function is_active(name)
+  return name == theme
+end
+
 return {
-	-- {
-	-- 	"nyoom-engineering/oxocarbon.nvim",
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		vim.opt.background = "dark" -- set this to dark or light
-	-- 		vim.cmd("colorscheme oxocarbon")
-	-- 	end,
-	-- }
+  {
+    "oskarnurm/koda.nvim",
+    lazy = not is_active("koda"),
+    priority = is_active("koda") and 1000 or nil,
+    config = function()
+      require("koda").setup()
+      if is_active("koda") then vim.cmd("colorscheme koda") end
+    end,
+  },
   {
     "vague-theme/vague.nvim",
-     lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other plugins
-  config = function()
-    -- NOTE: you do not need to call setup if you don't want to.
-    require("vague").setup({
-      -- optional configuration here
-    })
-    vim.cmd("colorscheme vague")
-  end
-  }
-	-- { "bettervim/yugen.nvim", lazy = false, priority = 1000 },
-	-- { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+    lazy = not is_active("vague"),
+    priority = is_active("vague") and 1000 or nil,
+    config = function()
+      require("vague").setup()
+      if is_active("vague") then vim.cmd("colorscheme vague") end
+    end,
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = not is_active("oxocarbon"),
+    priority = is_active("oxocarbon") and 1000 or nil,
+    config = function()
+      if is_active("oxocarbon") then vim.cmd("colorscheme oxocarbon") end
+    end,
+  },
+  {
+    "bettervim/yugen.nvim",
+    lazy = not is_active("yugen"),
+    priority = is_active("yugen") and 1000 or nil,
+    config = function()
+      if is_active("yugen") then vim.cmd("colorscheme yugen") end
+    end,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = not is_active("moonfly"),
+    priority = is_active("moonfly") and 1000 or nil,
+    config = function()
+      if is_active("moonfly") then vim.cmd("colorscheme moonfly") end
+    end,
+  },
 }
