772c3ab95e6a25b35c39fa51d2535043fc31b38a
BinBandit <crazywolf132@gmail.com>
2026-03-05 15:09:05 +1100

    perf(fish): cache keychain secrets and autoload loader function

--- dotfiles/config/fish/functions/__load_secret_from_keychain.fish
+++ dotfiles/config/fish/functions/__load_secret_from_keychain.fish
@@ -1,15 +1,30 @@
 function __load_secret_from_keychain --argument-names var service
+    # Dedup: if we already fetched this service, reuse the value.
+    set -l cache_var "__keychain_cache_"(string replace -a '.' '_' -- $service)
+    if set -q $cache_var
+        set -gx $var $$cache_var
+        return
+    end
+
     set -l value ""
     if type -q keychainctl
         set value (keychainctl get $service 2>/dev/null)
         if test $status -ne 0
             set value ""
         end
-    else if type -q security
-        set value (security find-generic-password -w -a $USER -s $service 2>/dev/null)
     end
-    
+
+    if test -z "$value"
+        if type -q security
+            set value (security find-generic-password -w -a "$USER" -s "$service" 2>/dev/null)
+            if test $status -ne 0; or test -z "$value"
+                set value (security find-generic-password -w -s "$service" 2>/dev/null)
+            end
+        end
+    end
+
     if test -n "$value"
+        set -g $cache_var $value
         set -gx $var $value
     end
 end
--- templates/fish/00-init.fish
+++ templates/fish/00-init.fish
@@ -38,31 +38,8 @@ if status is-interactive
 
 end
 
-# Keychain secret loader
-function __load_secret_from_keychain --argument-names var service
-    set -l value ""
-    if type -q keychainctl
-        set value (keychainctl get $service 2>/dev/null)
-        if test $status -ne 0
-            set value ""
-        end
-    end
-
-    if test -z "$value"
-        if type -q security
-            set value (security find-generic-password -w -a "$USER" -s "$service" 2>/dev/null)
-            if test $status -ne 0; or test -z "$value"
-                set value (security find-generic-password -w -s "$service" 2>/dev/null)
-            end
-        end
-    end
-    
-    if test -n "$value"
-        set -gx $var $value
-    end
-end
-
 # Keychain-backed env vars (needed for both interactive and non-interactive shells).
+# __load_secret_from_keychain lives in functions/ and is autoloaded by fish.
 if test -f $HOME/.config/fish/keychain-secrets.fish
     source $HOME/.config/fish/keychain-secrets.fish
 end
