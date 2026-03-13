55cdff39b05e9cfbfb6c2112d2ee2adda9671561
BinBandit <crazywolf132@gmail.com>
2026-03-13 18:55:17 +1100

    refactor(dotfiles-fish): simplify pr.fish to derive pr URL from current branch

--- dotfiles/config/fish/functions/pr.fish
+++ dotfiles/config/fish/functions/pr.fish
@@ -1,18 +1,27 @@
-function pr --description 'Open PR page on GitHub without gh'
-    # Ensure we're in a git repo
+function pr --description 'Open PR page on GitHub'
+    argparse 'f/fork' -- $argv
+    or return 1
+
     if not git rev-parse --git-dir >/dev/null 2>&1
         echo 'pr: Not in a git repository.' >&2
         return 1
     end
 
-    # Determine remote URL (prefer push URL for origin)
-    set -l url
-    if git remote get-url --push origin >/dev/null 2>&1
-        set url (git remote get-url --push origin)
-    else if git remote get-url origin >/dev/null 2>&1
-        set url (git remote get-url origin)
+    # Default to upstream when it exists (fork workflow), -f/--fork forces origin
+    set -l remote
+    if set -q _flag_fork
+        set remote origin
+    else if git remote get-url upstream >/dev/null 2>&1
+        set remote upstream
     else
-        set url (git remote -v | head -n1 | awk '{print $2}')
+        set remote origin
+    end
+
+    set -l url
+    if git remote get-url --push $remote >/dev/null 2>&1
+        set url (git remote get-url --push $remote)
+    else if git remote get-url $remote >/dev/null 2>&1
+        set url (git remote get-url $remote)
     end
 
     if test -z "$url"
@@ -20,7 +29,7 @@ function pr --description 'Open PR page on GitHub without gh'
         return 1
     end
 
-    # Normalize to HTTPS base URL suitable for browser
+    # Normalize to HTTPS base URL
     set -l base
     if string match -q 'git@*' -- $url
         set -l hostpath (string replace -r '^git@' '' -- $url)
@@ -39,54 +48,15 @@ function pr --description 'Open PR page on GitHub without gh'
     end
     set base (string replace -r '\\.git$' '' -- $base)
 
-    # Decide target URL
+    # Build PR search URL for current branch
     set -l prurl
-    if test (count $argv) -gt 0
-        set -l target $argv[1]
-        # If arg is a PR number (optionally prefixed with #)
-        set -l num (string replace -r '^#' '' -- $target)
-        if string match -qr '^[0-9]+$' -- $num
-            set prurl "$base/pull/$num"
-        else if test "$target" = 'list' -o "$target" = 'pulls'
-            set prurl "$base/pulls"
-        else if test "$target" = 'new' -o "$target" = 'create'
-            # Open compare page for current branch (best-effort; assumes main or master)
-            set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-            if test -z "$branch" -o "$branch" = 'HEAD'
-                set prurl "$base/pulls"
-            else
-                # Try to read local origin/HEAD to guess default branch; fallback to main then master
-                set -l default_branch
-                if test -e (git rev-parse --git-dir)/refs/remotes/origin/HEAD
-                    set -l ref (cat (git rev-parse --git-dir)/refs/remotes/origin/HEAD)
-                    set default_branch (string replace -r '^ref:\\s+refs/remotes/origin/' '' -- $ref)
-                end
-                if test -z "$default_branch"
-                    if git show-ref --verify --quiet refs/heads/main
-                        set default_branch main
-                    else if git show-ref --verify --quiet refs/heads/master
-                        set default_branch master
-                    else
-                        set default_branch main
-                    end
-                end
-                set prurl "$base/compare/$default_branch...$branch?expand=1"
-            end
-        else
-            # Treat as branch name: open search for open PRs with that head
-            set prurl "$base/pulls?q=is%3Apr+is%3Aopen+head%3A$target"
-        end
+    set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    if test -n "$branch" -a "$branch" != HEAD
+        set prurl "$base/pulls?q=is%3Apr+is%3Aopen+head%3A$branch"
     else
-        # No args: search for open PRs for current branch
-        set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-        if test -z "$branch" -o "$branch" = 'HEAD'
-            set prurl "$base/pulls"
-        else
-            set prurl "$base/pulls?q=is%3Apr+is%3Aopen+head%3A$branch"
-        end
+        set prurl "$base/pulls"
     end
 
-    # Open in default browser, or print URL
     if type -q open
         open $prurl
     else if type -q xdg-open
